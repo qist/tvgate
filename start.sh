e9a0d9076f3c631cf73e73ea485f8cad2a7b009e
qist <87984115@qq.com>
2026-09-14 14:27:43 +0800

    chore: 调整部署脚本二进制名与本地路径

--- start.sh
+++ start.sh
@@ -10,9 +10,9 @@
 #
 
 APP_NAME="tvgate"
-APP_DIR="/opt/tvgate"
+APP_DIR="/opt/tv-gate"
 APP_BIN="${APP_DIR}/build/TVGate-linux-64"
-CONFIG="${APP_DIR}/build/config.yaml"
+CONFIG="/opt/tvgate/build/config.yaml"
 PID_FILE="${APP_DIR}/${APP_NAME}.pid"
 LOG_FILE="/tmp/tvgate.log"
 
